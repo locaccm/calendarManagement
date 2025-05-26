159e109dfc8a3b5de2d78a68b47ef876804deee9
Dynastie AMOUSSOU <dynastie.amoussou@hotmail.com>
2025-05-26 17:40:07 +0200

    fix: [AIC-34] - Replace all logical OR operators with nullish coalescing operators

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import app from './app';
 import { AppDataSource } from './data-source';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 AppDataSource.initialize()
   .then(() => {
